4ce8d7486aaa672d23fd71bdcc1da5d91550f428
Josh Burdick <jburdick@keyfitz.org>
2016-01-29 12:17:23 -0500

    sketch, seems to work.

---
+++ summarize.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+# Summarizes results.
+
+use strict;
+
+print "# vertices,# gates,# random cases,wires sorted?,satisfiable?,CPU time\n";
+foreach my $f (<minisatOutput/*.txt>) {
+  my($skip, $n, $g, $random_cases, $wires_sorted) = split /_/, $f;
+  my $sat = "-";
+  my $t = "";
+
+  $wires_sorted = $wires_sorted eq "1" ? "yes" : "no";
+
+  open IN, "<$f" || die;
+  while(<IN>) {
+    chomp;
+    if (/^SATISFIABLE/) {
+      $sat = "yes";
+    }
+    if (/^UNSATISFIABLE/) {
+      $sat = "no";
+    }
+    if (/CPU time.*: (.*)/) {
+      $t = $1;
+    }
+  }
+
+  close IN;
+
+  print join ",", ($n, $g, $random_cases, $wires_sorted, $sat, $t);
+  print "\n";
+}
+
+
